5adc6f0d658473d6cf3016ae52baeef3718e9591
harshdixit121 <21cse140@glbitm.ac.in>
2024-12-01 22:44:47 +0530

    Form Created Login

--- frontend/src/pages/Form.js
+++ frontend/src/pages/Form.js
@@ -1,75 +1,79 @@
+import React from "react";
 import "./Form.css";
 
 function Form() {
   return (
     <div className="container">
-      <h1>Truck Dost</h1>
-      <form className="form">
+      <h1 className="form-title">Truck Dost Application Form</h1>
+      <form className="styled-form">
+        {/* Name Field */}
         <div className="form-group">
-          <label htmlFor="firstname">First Name</label>
+          <label htmlFor="name">Name</label>
           <input
             type="text"
-            placeholder="Enter First Name"
-            name="firstname"
-            id="firstname"
+            id="name"
+            name="name"
+            placeholder="Enter Your Name"
+            required
           />
         </div>
 
+        {/* Mobile Number Field */}
         <div className="form-group">
-          <label htmlFor="lastname">Last Name</label>
+          <label htmlFor="mobileNumber">Mobile Number</label>
           <input
             type="text"
-            placeholder="Enter Last Name"
-            name="lastname"
-            id="lastname"
+            id="mobileNumber"
+            name="mobileNumber"
+            placeholder="Enter Your Number"
+            required
+            pattern="^\d{10}$"
           />
         </div>
 
+        {/* Password Field */}
         <div className="form-group">
           <label htmlFor="password">Password</label>
           <input
             type="password"
-            placeholder="Enter Password"
-            name="password"
             id="password"
+            name="password"
+            placeholder="Enter Password"
+            required
           />
         </div>
 
+        {/* Truck Capacity Field */}
         <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            placeholder="Enter Email"
-            name="email"
-            id="email"
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="contact">Contact</label>
+          <label htmlFor="truckCapacity">Truck Capacity</label>
           <input
             type="text"
-            placeholder="Enter your Contact"
-            name="contact"
-            id="contact"
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="truckcapacity">Truck Capacity</label>
-          <input
-            type="text"
+            id="truckCapacity"
+            name="truckCapacity"
             placeholder="Enter Truck Capacity"
-            name="truckcapacity"
-            id="truckcapacity"
+            required
           />
         </div>
 
+        {/* License Plate Number Field */}
+        <div className="form-group">
+          <label htmlFor="licensePlateNumber">License Plate Number</label>
+          <input
+            type="text"
+            id="licensePlateNumber"
+            name="licensePlateNumber"
+            placeholder="Enter License Plate Number"
+            required
+          />
+        </div>
+
+        {/* Date Field */}
         <div className="form-group">
           <label htmlFor="date">Date</label>
-          <input type="date" name="date" id="date" />
+          <input type="date" id="date" name="date" required />
         </div>
 
+        {/* Buttons */}
         <div className="form-buttons">
           <button type="reset" className="reset-btn">
             Reset
