effe24e87f249462b42ce91fbce0c6d205c624c6
harshdixit121 <21cse140@glbitm.ac.in>
2024-12-01 22:04:14 +0530

    add

--- frontend/pages/Form.js
+++ frontend/pages/Form.js
@@ -1,8 +1,11 @@
+import "./Form.css";
+
 function Form() {
-    return (
-      <div className="container">
-        <h1>Truck Dost</h1>
-        <form>
+  return (
+    <div className="container">
+      <h1>Truck Dost</h1>
+      <form className="form">
+        <div className="form-group">
           <label htmlFor="firstname">First Name</label>
           <input
             type="text"
@@ -10,7 +13,9 @@ function Form() {
             name="firstname"
             id="firstname"
           />
-  
+        </div>
+
+        <div className="form-group">
           <label htmlFor="lastname">Last Name</label>
           <input
             type="text"
@@ -18,7 +23,9 @@ function Form() {
             name="lastname"
             id="lastname"
           />
-  
+        </div>
+
+        <div className="form-group">
           <label htmlFor="password">Password</label>
           <input
             type="password"
@@ -26,7 +33,9 @@ function Form() {
             name="password"
             id="password"
           />
-  
+        </div>
+
+        <div className="form-group">
           <label htmlFor="email">Email</label>
           <input
             type="email"
@@ -34,7 +43,9 @@ function Form() {
             name="email"
             id="email"
           />
-  
+        </div>
+
+        <div className="form-group">
           <label htmlFor="contact">Contact</label>
           <input
             type="text"
@@ -42,21 +53,34 @@ function Form() {
             name="contact"
             id="contact"
           />
-  
-          <label htmlFor="date">Date</label>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="truckcapacity">Truck Capacity</label>
           <input
-            type="date"
-            placeholder="Enter Date"
-            name="date"
-            id="date"
+            type="text"
+            placeholder="Enter Truck Capacity"
+            name="truckcapacity"
+            id="truckcapacity"
           />
-  
-          <button type="reset">Reset</button>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
-  }
-  
-  export default Form;
-  
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="date">Date</label>
+          <input type="date" name="date" id="date" />
+        </div>
+
+        <div className="form-buttons">
+          <button type="reset" className="reset-btn">
+            Reset
+          </button>
+          <button type="submit" className="submit-btn">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
